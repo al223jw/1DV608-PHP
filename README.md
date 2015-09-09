5eaae632b60c128a6d31e0ea1d67ac0e6da98f14
Anton Larsson <al223jw@student.lnu.se>
2015-09-09 11:38:18 +0200

    Fitsr commit for Assignment 2.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Login_1DV608
+Interface repository for 1DV608 assignment 2 and 4
+
+Here i will be writing the documentation to my assignment.
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+//INCLUDE THE FILES NEEDED...
+require_once('view/LoginView.php');
+require_once('view/DateTimeView.php');
+require_once('view/LayoutView.php');
+
+//MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
+//CREATE OBJECTS OF THE VIEWS
+$v = new LoginView();
+$dtv = new DateTimeView();
+$lv = new LayoutView();
+
+echo "Just a test string";
+
+$lv->render(false, $v, $dtv);
+
